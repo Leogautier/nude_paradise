b2eee01c8b54fb1ab1856ea19c18377b445f42d6
Mratsoo <mratsoo@gmail.com>
2022-10-20 10:44:39 +0200

    routes

---
+++ src/Screens/AcceuilScreen.jsx
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const AcceuilScreen = () => {
+    return (
+        <div>
+            <h1>
+                Acceuil
+            </h1>
+        </div>
+    );
+};
+
+export default AcceuilScreen;
